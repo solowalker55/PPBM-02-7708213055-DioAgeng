4d377add0f6443b36d7746e7a63eb1a62e05f543
Dio Setiawan <solowalker8898@gmail.com>
2023-11-25 15:54:14 +0700

    Saved 6
    Implemented Double tap for back button in Home activity for exit confirmation

--- ListViewTest/app/src/main/java/com/dioageng/myapplication/Home.kt
+++ ListViewTest/app/src/main/java/com/dioageng/myapplication/Home.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 
 class Home : AppCompatActivity() {
+
+    private var lastBackPressTime: Long = 0
+    private val doublePressThreshold: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -37,6 +41,19 @@ class Home : AppCompatActivity() {
         goToCustomListView.setOnClickListener{
             val Intent = Intent(this, CustomList::class.java)
             startActivity(Intent)
+        }
+    }
+
+    override fun onBackPressed() {
+        // Check if it's a double press within the threshold
+        if (System.currentTimeMillis() - lastBackPressTime < doublePressThreshold) {
+            // Double press detected, exit the app
+            finishAffinity()
+        } else {
+            // Update the last back press time
+            lastBackPressTime = System.currentTimeMillis()
+            // Show a toast or any other indication for the first press
+            Toast.makeText(this, "Press back again to exit", Toast.LENGTH_SHORT).show()
         }
     }
 }
