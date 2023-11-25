3a610ce0b247dc431f22173e23a90bfe8ee2adb3
Dio Setiawan <solowalker8898@gmail.com>
2023-11-25 14:55:19 +0700

    added custom kotlin and its xml

---
+++ ListViewTest/app/src/main/java/com/dioageng/myapplication/CustomList.kt
@@ -0,0 +1,11 @@
+package com.dioageng.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_list)
+    }
+}
