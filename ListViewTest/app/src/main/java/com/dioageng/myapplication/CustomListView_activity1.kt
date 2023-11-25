d115341773e2983915327395e8c57c21a2af5cf0
Dio Setiawan <solowalker8898@gmail.com>
2023-11-25 14:58:26 +0700

    added customlistview_activity1 and its activity

---
+++ ListViewTest/app/src/main/java/com/dioageng/myapplication/CustomListView_activity1.kt
@@ -0,0 +1,11 @@
+package com.dioageng.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomListView_activity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_list_view1)
+    }
+}
